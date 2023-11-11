ab51eb4be252520012dbec735d75f48670a13820
Arka Bose <arkaprabhobose@gmail.com>
2023-11-11 13:31:40 -0600

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+'use client'
 import Image from 'next/image'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  type ResponseData = {
+    message: string
+  }
+   
+
+  const val = fetch('http://localhost:3000/api/')
+
+  useEffect(() => {
+    val.then((response) => {
+      console.log(response)
+    })
+  }
+  )
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
