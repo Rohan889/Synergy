9e20f5bb52a4fbd6bc5d15a60c48d51697eebb89
Sriabhi Venkat <sriabhi01@gmail.com>
2023-11-28 20:48:03 -0600

    gptContext function uploaded

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,42 +10,61 @@ type ResponseData = {
 
 const llm = new OpenAI({
   openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
-  maxTokens: 100,
+  maxTokens: 175,
 });
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("search");
+export interface Job {
+  title: string;
+  loc: string;
+  highlights?: any;
+}
 
-  const data = await fetch(
-    `http://localhost:3000/api`
-  , {method: "POST", body: JSON.stringify({search: id}), headers: {"Content-Type": "application/json"}});
-  const dataJson = await data.json();
-
-
+export async function gptContext(data: Job) {
+  
   const response = await llm.call(
-    "What are the most prevalent skills and average salary required for this job given this job description: " +
-      dataJson.response[0].title + " Qualifications: " + dataJson.response[0].highlights["items"], 
+    "Within 150 characters, what are the most prevalent skills and average salary required for this job given this job description: " +
+      data.title +
+      " Qualifications: " +
+      data.highlights[0].items.join(" ") + 
+      " Responsibilities: " +
+      data.highlights[1].items.join(" ") +
+      " Format salary as a number at the end."
   );
 
-  return Response.json({
-    response,
-  });
+  return response;
 }
+
+// export async function GET(request: Request) {
+
+//   const response = await gptContext();
+
+//   return Response.json({
+//     response: response,
+//   });
+// }
 
 export async function POST(request: Request) {
   const object = await request.json();
   var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+    method: "GET",
+    redirect: "follow",
   };
-  
-  const data = await fetch(`https://serpapi.com/search.json?engine=google_jobs&q=${object.search}&api_key=312e4ac293ce6ece05ec54c150ecc09ff635f2441331b8e14ba8671f136ab0c6`, requestOptions)
+
+  const data = await fetch(
+    `https://serpapi.com/search.json?engine=google_jobs&q=${object.search}&api_key=312e4ac293ce6ece05ec54c150ecc09ff635f2441331b8e14ba8671f136ab0c6`,
+    requestOptions
+  );
   const dataJson = await data.json();
 
-  console.log(dataJson)
+  console.log(dataJson);
 
   return Response.json({
-    response: dataJson["jobs_results"].slice(0,3).map((job: any) => ({title: job.title, loc: job.location, highlights: job.job_highlights}))
+    response: dataJson["jobs_results"]
+      .slice(0, 3)
+      .map((job: any) => ({
+        title: job.title,
+        loc: job.location,
+        highlights: job.job_highlights,
+      })),
   });
 }
